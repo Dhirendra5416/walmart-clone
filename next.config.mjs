ce541df512445ca619d3714f12c1f9ab42eb9429
Dhirendra5416 <dhirendrapratap28maurya@gmail.com>
2024-02-21 20:40:13 +0530

    Header

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,25 @@
+
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images:{
+        remotePatterns:[
+           {
+            protocol:'https',
+            hostname:'links.papareact.com',
+            // port:'',
+            // pathname:'/account123/**'
+            },
+            {
+                protocol:'https',
+                hostname:'i.imgur.com',
+            },
+            {
+                protocol:'https',
+                hostname:'i5.walmartimages.com',
+            }
+        ]
+    }
+};
 
 export default nextConfig;
